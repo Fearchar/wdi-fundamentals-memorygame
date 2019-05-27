ccf5b96c095e753215d90f217ff919e611f17a1d
FNM <fearchar.maclean@gmail.com>
2019-05-27 15:07:08 +0100

    created game logic.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,28 @@
 var cards = ["queen","queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
-}
+
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
+};
+
+function flipCard(cardId) {
+  console.log("User flipped " +  cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+/* Note to GA: Slide 9 of the "Project: Functions (JavaScript)" shows that the console should be logging the following:
+      User flipped queen
+      User flipped king
+      Sorry, try again.
+  
+  However, the instructions to put the checkForMatch call at the end of the flipCard function means that "Sorry, try again" 
+  is printed after the first flipCard call as well as after the second.
+*/
