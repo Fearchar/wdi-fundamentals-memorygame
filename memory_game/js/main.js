993f43a6228a843e46c649d93a2265776ca78de8
FNM <fearchar.maclean@gmail.com>
2019-05-30 15:06:49 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,28 +1,43 @@
-var cards = ["queen","queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen", 
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen", 
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king", 
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king", 
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    alert("You found a match!");
   } else {
-    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
   }
 };
 
 function flipCard(cardId) {
-  console.log("User flipped " +  cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " +  cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
 };
 
 flipCard(0);
 flipCard(2);
 
-/* Note to GA: Slide 9 of the "Project: Functions (JavaScript)" shows that the console should be logging the following:
-      User flipped queen
-      User flipped king
-      Sorry, try again.
-  
-  However, the instructions to put the checkForMatch call at the end of the flipCard function means that "Sorry, try again" 
-  is printed after the first flipCard call as well as after the second.
-*/
